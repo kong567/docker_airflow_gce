f5ccb4cad2631a0ec86b590ee9cebb234f3e20ff
kong <david.5516kimo@gmail.com>
2025-08-15 01:26:22 +0800

    3

--- src/dataflow/dags/ETF_PremiumDiscount.py
+++ src/dataflow/dags/ETF_PremiumDiscount.py
@@ -25,7 +25,7 @@ with airflow.DAG(
     # 套用預設參數設定
     default_args=DEFAULT_ARGS,
     # 不自動排程，只能手動或外部觸發
-    schedule_interval="0 13-15 * * 1-5",
+    schedule_interval="0 8 * * 1-5",
     # schedule_interval= None,
     concurrency=6,
     # 限制同時執行的最大 DAG 實例數
