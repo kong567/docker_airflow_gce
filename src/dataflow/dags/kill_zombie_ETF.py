25d1e1e1fec4d299f032d3a04e91c66ebc49e68c
kong <david.5516kimo@gmail.com>
2025-08-21 01:56:45 +0800

    2

--- src/dataflow/dags/kill_zombie_ETF.py
+++ src/dataflow/dags/kill_zombie_ETF.py
@@ -1,22 +1,24 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from airflow.utils.dates import days_ago
+from pendulum import timezone 
 from datetime import datetime
 from dataflow.etl.kill_zom import (
     # 建立並回傳一個 DockerOperator 任務
     clear_stuck_dag_runs_ETF,
 )
-today = datetime.now().strftime('%Y%m%d')
+
+local_tz = timezone("Asia/Taipei")
+# today = datetime.now().strftime('%Y%m%d')
 
 with DAG(
     dag_id="maintenance_clear_stuck_runs_ETF",
-    start_date=days_ago(1),
+    start_date=datetime(2022, 1, 1, tzinfo=local_tz),
     schedule_interval="*/30 8-10 * * 1-5",  # 每 20 分鐘掃一次
     catchup=False,
     max_active_runs=1,
 ) as dag:   
     clear_stuck = PythonOperator(
-    task_id=f"clear_stuck_dag_runs_ETF_{today}",
+    task_id="clear_stuck_dag_runs_ETF",
     python_callable=clear_stuck_dag_runs_ETF,
     queue = "All_crawler",
 )
--- src/dataflow/dags/kill_zombie_vix_or_News.py
+++ src/dataflow/dags/kill_zombie_vix_or_News.py
@@ -1,22 +1,24 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from airflow.utils.dates import days_ago
 from datetime import datetime
+from pendulum import timezone 
 from dataflow.etl.kill_zom import (
     # 建立並回傳一個 DockerOperator 任務
     clear_stuck_dag_runs_NEWS,
 )
-today = datetime.now().strftime('%Y%m%d')
+
+local_tz = timezone("Asia/Taipei")
+# today = datetime.now().strftime('%Y%m%d')
 
 with DAG(
     dag_id="maintenance_clear_stuck_runs_NEWS",
-    start_date=days_ago(1),
+    start_date=datetime(2022, 1, 1, tzinfo=local_tz),
     schedule_interval="50 23 * * *",  # 每 20 分鐘掃一次
     catchup=False,
     max_active_runs=1,
 ) as dag:   
     clear_stuck = PythonOperator(
-    task_id=f"clear_stuck_dag_runs_NEWS_{today}",
+    task_id="clear_stuck_dag_runs_NEWS",
     python_callable=clear_stuck_dag_runs_NEWS,
     queue = "All_crawler",
 )
